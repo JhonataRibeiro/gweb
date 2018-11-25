24445bf11bef4fb39e4afd98982c7f3693708857
jhonataribeiro <jhonata.ribeiro.ti@gmail.com>
2018-11-25 18:59:46 -0200

    Adiciona jpql

--- src/main/java/br/com/crud/gweb/controller/UsuarioController.java
+++ src/main/java/br/com/crud/gweb/controller/UsuarioController.java
@@ -23,7 +23,7 @@ public class UsuarioController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity listar() {
         try {
-            return new ResponseEntity(usuarioService.obterUsuarios(), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.obterUsuarios(), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -32,7 +32,7 @@ public class UsuarioController {
     @RequestMapping(value = "/{id}/documentos", method = RequestMethod.GET)
     public ResponseEntity obteResponseEntity(@PathVariable("id") Long id, @PathParam("dataInicio") Date dataInicio, @PathParam("dataInicio") Date dataFim) {
         try {
-            return new ResponseEntity(usuarioService.obterDocumentsPorRangeData(id, dataInicio, dataFim), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.obterDocumentsPorRangeData(id, dataInicio, dataFim), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -41,7 +41,7 @@ public class UsuarioController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity inserir(@RequestBody Usuario usuario) {
         try {
-            return new ResponseEntity(usuarioService.salvarUsuario(usuario), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.salvarUsuario(usuario), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -51,7 +51,7 @@ public class UsuarioController {
     @RequestMapping(method = RequestMethod.PUT)
     public ResponseEntity editar(@RequestBody Usuario usuario) {
         try {
-            return new ResponseEntity(usuarioService.editarUsuario(usuario), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.editarUsuario(usuario), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -60,7 +60,7 @@ public class UsuarioController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity deletar(@PathVariable("id") Long id) {
         try {
-            return new ResponseEntity(usuarioService.exlcuirUsuario(id), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.exlcuirUsuario(id), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
@@ -69,7 +69,7 @@ public class UsuarioController {
     @RequestMapping(value = "/{id}/documento/{idDocumento}", method = RequestMethod.DELETE)
     public ResponseEntity deletar(@PathVariable("id") Long id, @PathVariable("idDocumento") Long idDocumento) {
         try {
-            return new ResponseEntity(usuarioService.excluirDocumento(id, idDocumento), HttpStatus.OK);
+            return new ResponseEntity<>(usuarioService.excluirDocumento(id, idDocumento), HttpStatus.OK);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
--- src/main/java/br/com/crud/gweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/crud/gweb/repository/UsuarioRepository.java
@@ -2,11 +2,15 @@ package br.com.crud.gweb.repository;
 
 import br.com.crud.gweb.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author: Jhonata Ribeiro
  * @date: 23/11/18
  */
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    @Query("select u from Usuario u where u.id = ?1")
     Usuario findAById(Long id);
+
 }
